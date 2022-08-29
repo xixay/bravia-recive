007546d67bf42f459fdca10f07aebb96946e6f26
richard.teran <richard.teran@agetic.gob.bo>
2022-08-29 08:41:50 -0400

    feat :100: consume el mensaje de la cola

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//En receive.js , primero debemos solicitar la biblioteca:
+import * as amqp from 'amqplib';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  connect();
   await app.listen(3001);
 }
 bootstrap();
+async function connect() {
+  try {
+    //luego conéctese al servidor RabbitMQ
+    // const connection = await amqp.connect("amqp://localhost:5672");
+    const coneccion = await amqp.connect('amqp://guest:guest@localhost:5672');
+    //A continuación, creamos un canal
+    const canal = await coneccion.createChannel();
+    const cola = 'cola_de_sms';
+    await canal.assertQueue(cola);
+    canal.consume(cola, (mensaje) => {
+      const input = JSON.parse(mensaje.content.toString());
+      // const input = message.content.toString();
+      console.log(' [x] Recibido %s', input);
+      canal.ack(mensaje);
+    });
+    console.log(
+      ' [*] Esperando lo enviado por %s. Para salir presione CTRL+C',
+      cola,
+    );
+  } catch (ex) {
+    console.error(ex);
+  }
+}
